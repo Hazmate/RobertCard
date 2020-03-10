c6ddca02ce48c32613ccfc25946c8f20823f5884
Robert Hatzmann <robert.hatzmann@gmx.de>
2020-03-10 10:05:03 +0100

    updated gitignore

---
+++ RobertCard/InfoView.swift
@@ -0,0 +1,21 @@
+//
+//  InfoView.swift
+//  RobertCard
+//
+//  Created by Robert Hatzmann on 02.03.20.
+//  Copyright © 2020 Robert Hatzmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
